f443028f4df93c5b042ebc9593a804a33b696e1a
themikesmith <themikesmith+github@gmail.com>
2013-11-24 18:04:18 -0500

    initial commit

---
+++ edu/jhu/cs/elanmike/CollapsedGibbsSampler.java
@@ -0,0 +1,13 @@
+package edu.jhu.cs.elanmike;
+
+public class CollapsedGibbsSampler {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
