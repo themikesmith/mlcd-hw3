79f8cdddee14120956e31865feb978278e9f6073
Elan <mail@elanhr.org>
2013-11-24 20:19:00 -0500

    Starting to put in HashStuff

--- edu/jhu/cs/elanmike/GibbsSampler.java
+++ edu/jhu/cs/elanmike/GibbsSampler.java
@@ -1,7 +1,20 @@
 package edu.jhu.cs.elanmike;
 
+import java.util.HashMap;
+
 public class GibbsSampler {
 
+	private HashMap<String,Integer> WordToIndex;
+	
+	GibbsSampler(){
+		WordToIndex = new HashMap<String,Integer>();
+	}
+	
+	
+	private void processWord(String word){
+		
+	}
+	
 	/**
 	 * @param args
 	 */
@@ -18,7 +31,7 @@ public class GibbsSampler {
 		double lambda = Double.parseDouble(args[4]), alpha = Double.parseDouble(args[5]), 
 				beta = Double.parseDouble(args[6]);
 	}
-	private void usage() {
+	private static void usage() {
 		System.out.println("Usage:./collapsed-sampler trainFile testFile outputFile K lambda alpha beta totalNumSamples totalBurnIn\n" +
 				"Eample usage:\n./collapsed-sampler input-train.txt " +
 				"input-test.txt output.txt 10 0.5 0.1 0.01 1100 1000\n" +
