34426ba610f7022a7e1ecb8e79d5fe8d5e464e65
themikesmith <themikesmith+github@gmail.com>
2013-11-24 20:53:22 -0500

    add optional '-b' argument for blocked sampling, add creation of sampler in main, fix usage

--- edu/jhu/cs/elanmike/GibbsSampler.java
+++ edu/jhu/cs/elanmike/GibbsSampler.java
@@ -90,25 +90,37 @@ public class GibbsSampler {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		if(args.length != 9) {
+		SamplerType type = SamplerType.COLLAPSED;
+		if(args.length < 9 || args.length > 10) {
 			usage();
 			return;
 		}
+		else if(args.length == 10) {
+			if(args[9].equals("-b")) {
+				type = SamplerType.BLOCKED;
+			}
+			else {
+				usage();
+				return;
+			}
+		}
 		// read arguments
 		String trainingFile = args[0], testFile = args[1], outFile = args[2];
-		int k = Integer.parseInt(args[3]), totalIters = Integer.parseInt(args[7]), 
+		int numTopics = Integer.parseInt(args[3]), totalIters = Integer.parseInt(args[7]), 
 				totalBurnin = Integer.parseInt(args[8]);
 		double lambda = Double.parseDouble(args[4]),
 				alpha = Double.parseDouble(args[5]), 
 				beta = Double.parseDouble(args[6]);
-		
+		GibbsSampler g = new GibbsSampler(type, 2, numTopics, lambda, alpha, beta);
 		
 	}
 	private static void usage() {
 		System.out.println("Usage:./collapsed-sampler trainFile testFile outputFile K lambda alpha beta totalNumSamples totalBurnIn\n" +
-				"Eample usage:\n./collapsed-sampler input-train.txt " +
+				"Example usage:\n./collapsed-sampler input-train.txt " +
 				"input-test.txt output.txt 10 0.5 0.1 0.01 1100 1000\n" +
-				"This runs the sampler with K = 10 topics and would" +
-				"collect 100 samples after a 1000-iteration burn-in.");
+				"This runs the sampler with K = 10 topics and would " +
+				"collect 100 samples after a 1000-iteration burn-in.\n" +
+				"An optional 10th argument may be passed '-b' to run blocked collapsed gibbs sampling" +
+				" instead of the default collapsed gibbs");
 	}
 }
